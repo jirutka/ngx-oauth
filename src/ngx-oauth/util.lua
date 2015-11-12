3b5ff53cdb63ed61fd11a9e74619a573296de5f4
Jakub Jirutka <jakub@jirutka.cz>
2015-11-12 03:42:25 +0100

    Improve util.merge to accept more than two tables

--- src/ngx-oauth/util.lua
+++ src/ngx-oauth/util.lua
@@ -38,13 +38,24 @@ function M.is_blank (value)
   return value == nil or value == '' or tostring(value):find('^%s*$') ~= nil
 end
 
---- Returns a new table containing the contents of tables `tab1` and `tab2`.
--- Entries with duplicate keys are overwritten with the values from `tab2`.
-function M.merge (tab1, tab2)
-  local tab3 = {}
-  for k, v in pairs(tab1) do tab3[k] = v end
-  for k, v in pairs(tab2) do tab3[k] = v end
-  return tab3
+--- Returns a new table containing the contents of all the given tables.
+-- Tables are iterated using @{pairs}, so this function is intended for tables
+-- that represent *associative arrays*. Entries with duplicate keys are
+-- overwritten with the values from a later table.
+--
+-- @tparam {table,...} ... The tables to merge.
+-- @treturn table A new table.
+-- @see concat
+function M.merge (...)
+  local result = {}
+
+  for _, tab in ipairs {...} do
+    for key, val in pairs(tab) do
+      result[key] = val
+    end
+  end
+
+  return result
 end
 
 --- Partial application.
