49327777e410e07fe3c277d80ba448621b414d3d
Jakub Jirutka <jakub@jirutka.cz>
2015-11-20 19:38:28 +0100

    Add userauth script for nginx

---
+++ src/ngx-oauth-userauth.lua
@@ -0,0 +1,77 @@
+---------
+-- User authorization script for OAuth 2.0 grant authorization code.
+
+local config  = require 'ngx-oauth.config'
+local Cookies = require 'ngx-oauth.Cookies'
+local ethr    = require 'ngx-oauth.either'
+local httpc   = require 'ngx-oauth.http_client'
+local nginx   = require 'ngx-oauth.nginx'
+local oauth   = require 'ngx-oauth.oauth2'
+local util    = require 'ngx-oauth.util'
+
+local par      = util.partial
+local either   = ethr.either
+local redirect = ngx.redirect
+
+local fail_with_oaas_error = par(nginx.fail, 503, 'Authorization server error: %s')
+local get_or_fail = par(either, fail_with_oaas_error, util.id)
+
+
+local conf, err = config.load()
+if err then
+  return nginx.fail(500, 'OAuth proxy error: %s', err)
+end
+
+local cookies = Cookies(conf)
+
+local log_info  = par(nginx.log, ngx.INFO)
+local log_debug = function() end
+
+if conf.debug then
+  log_debug = par(nginx.log, ngx.DEBUG)
+end
+
+-- Got response from the authorization server.
+if ngx.var.uri == conf.redirect_path then
+  log_debug('processing request from authorization server')
+
+  local request_args = ngx.req.get_uri_args()
+  local auth_code = request_args.code
+
+  if request_args.error then
+    return nginx.fail(403, request_args.error)
+
+  elseif auth_code then
+    log_debug('requesting token for auth code: %s', auth_code)
+
+    local token = get_or_fail(oauth.request_token('authorization_code', conf, auth_code))
+    cookies.add_token(token)
+
+    local user = get_or_fail(httpc.get_for_json(conf.userinfo_url, token.access_token))
+    cookies.add_username(user.username)
+
+    log_info("authorized user '%s', redirecting to: %s", user.username, conf.success_path)
+    return redirect(conf.success_path)
+
+  else
+    return nginx.fail(400, "Missing query parameter 'code' or 'error'.")
+  end
+
+-- Cookie with refresh token found, requesting a new access token.
+elseif cookies.get_refresh_token() then
+  log_info('refreshing token for user: %s', cookies.get_username())
+
+  util.pipe({
+    cookies.get_refresh_token,
+    par(oauth.request_token, 'refresh_token', conf),
+    get_or_fail,
+    cookies.add_token
+  })()
+
+  return redirect(conf.success_path)
+
+-- Cookie with refresh token not found, redirecting to the authorization endpoint.
+else
+  log_info('redirecting to authorization endpoint')
+  return redirect(oauth.authorization_url(conf))
+end
